c2145f7e27c7d77fda41e3418c796fded9abae6b
yann1294 <36128975+yann1294@users.noreply.github.com>
2020-07-22 16:05:32 +0530

    .gitignore added

--- salazard_list/urls.py
+++ salazard_list/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from my_app import views
 
 urlpatterns = [
-    path('', include('my_app.urls') ),
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
